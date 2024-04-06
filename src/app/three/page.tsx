a4f161246f2e398715dbe2ae7f33a32cff207536
exisodd <ethantran.mail@gmail.com>
2024-04-06 05:50:10 -0700

    added letter a in gltf format

--- src/app/three/page.tsx
+++ src/app/three/page.tsx
@@ -1,34 +1,56 @@
 "use client";
 
-import * as THREE from 'three'
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei";
-import Box from '@/components/Box3D'
-import { Paper } from '@mantine/core';
-// import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { CameraControls } from "@react-three/drei";
+import { Paper } from "@mantine/core";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useRef } from "react";
 
-const Scene = () => {
-  const stl = useLoader(FBXLoader, "qq.fbx");
+const Scene = ({ letter }: { letter: string }) => {
+  const stl: any = useLoader(GLTFLoader, `${letter}.gltf`);
+  // const stl = useLoader(STLLoader, `abb ${letter} print.STL`);
 
-  return <primitive object={stl} scale={1} position={[0, 0, 0]} />;
+  return <primitive object={stl.scene} scale={0.3} position={[0, 0, 0]} />;
 };
 
 export default function Three() {
-    // const model = useLoader(STLLoader, 'abb O print.STL');
+  const cameraRef = useRef<CameraControls | null>(null!);
+  // const model = useLoader(STLLoader, 'abb O print.STL');
 
-    return (
-        <Paper h="80vh">            
-            <Canvas>
-                <OrbitControls />
-                <Scene />
-                <ambientLight intensity={Math.PI / 2} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-                <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-                <Box position={[0, 0, 0]} />
-                {/* <primitive object={model} /> */}
-            </Canvas>
-        </Paper>
-    )
+  function initLocAndRot() {
+    if (!cameraRef.current) return;
+    // POS (x,y,z) + LOOK POS (x,y,z)
+    cameraRef.current.setLookAt(0, 0, 30, 0, 0, 0);
+  }
+
+  return (
+    <Paper h="90vh">
+      <Canvas camera={{ fov: 90 }}>
+        <Scene letter={"a"} />
+        <CameraControls
+          ref={(ref) => {
+            cameraRef.current = ref;
+            if (ref) {
+              initLocAndRot();
+            }
+          }}
+        />
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[0, 30, -30]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[30, 0, 30]} decay={0} intensity={Math.PI} />
+        <pointLight position={[-30, 0, -30]} decay={0} intensity={Math.PI} />
+        <pointLight position={[30, 0, -30]} decay={0} intensity={Math.PI} />
+        <pointLight position={[-30, 0, 30]} decay={0} intensity={Math.PI} />
+        {/* This box is actually at the 0,0,0 position */}
+        {/* <Box position={[0, 0, 0]} /> */}
+      </Canvas>
+    </Paper>
+  );
 }
