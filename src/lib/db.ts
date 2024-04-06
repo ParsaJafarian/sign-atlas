2d0bc5385d6841cb0408576f304d3e7699025fed
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-04-05 22:59:25 -0400

    Add basic database connection

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
