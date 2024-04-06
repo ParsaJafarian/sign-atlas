b27f0009b9169955547d688efd819f2aff4a3680
Kaidali Sohaib <kaidalisohaib@gmail.com>
2024-04-06 06:39:11 -0400

    connected courses and tests in the /courses page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,57 +1,73 @@
 import CourseCard from "@/components/CourseCard";
 import { ProgressCard } from "@/components/ProgressCard";
 import { UserButton } from "@/components/UserButton";
-import { Container, Title, Box, Stack, Card } from "@mantine/core";
+import {
+  Container,
+  Title,
+  Box,
+  Stack,
+  Card,
+  Group,
+  Paper,
+  Center,
+  Button,
+} from "@mantine/core";
+import Link from "next/link";
 
 export default function Courses() {
-
-  interface ModuleList {
-    [key: string]: boolean;
-  }
-
-  interface Course {
+  interface CourseCardProps {
     title: string;
     description: string;
-    modules: ModuleList;
+    lessons: string[];
+    completedLessons: string[];
+    courseEndpoint: string;
+    testEndpoint: string;
   }
 
-  const mockData: Course[] = [
+  const mockData: CourseCardProps[] = [
     {
       title: "From A to M",
-      description: "Learn the American Sign Language from A to M!",
-      modules: {
-        a: false,
-        b: false,
-        c: false,
-        d: false,
-        e: false,
-        f: false,
-        g: false,
-        h: false,
-        i: false,
-        j: false,
-        k: false,
-        l: false,
-        m: false,
-      },
+      description: "Learn the American Sign Language from A to J!",
+      lessons: [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+      ],
+      completedLessons: ["a", "b"],
+      courseEndpoint: "a-m",
+      testEndpoint: "a-m",
     },
     {
-      title: "From N to Z",
+      title: "From K to Z",
       description: "Continue learning the American Sign Language from K to Z!",
-      modules: {
-        n: false,
-        o: false,
-        p: false,
-        q: false,
-        r: false,
-        t: false,
-        u: false,
-        v: false,
-        w: false,
-        x: false,
-        y: false,
-        z: false,
-      },
+      lessons: [
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+      ],
+      completedLessons: ["n", "o", "p", "q"],
+      courseEndpoint: "n-z",
+      testEndpoint: "n-z",
     },
   ];
 
@@ -61,16 +77,32 @@ export default function Courses() {
       <Box mt="lg" h="60%">
         <Title>My courses</Title>
         <ProgressCard mt="md" />
-        <Card h="100%" radius="md" px="xl" mt="md" withBorder bg="blue">
-          <Stack h="100%" gap="xl" justify="center">
+        <Card radius="md" px="xl" mt="md" withBorder bg="blue">
+          <Stack gap="xl" justify="center">
             {mockData.map((course, index) => (
               <CourseCard
                 key={index}
                 title={course.title}
                 description={course.description}
-                modules={course.modules}
+                lessons={course.lessons}
+                completedLessons={course.completedLessons}
+                courseEndpoint={"a-m"}
+                testEndpoint={course.testEndpoint}
               />
             ))}
+            <Paper h="130px" radius="md" shadow="xl" py={0}>
+              <Link href={"/tests/colors"}>
+                <Button
+                  w={"100%"}
+                  h={"100%"}
+                  // p={50}
+                  variant="gradient"
+                  gradient={{ from: "pink", to: "orange", deg: 90 }}
+                >
+                  <Title>Extra ✨ Spell Common Colors</Title>
+                </Button>
+              </Link>
+            </Paper>
           </Stack>
         </Card>
       </Box>
