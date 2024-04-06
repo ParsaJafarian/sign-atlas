8ddfede39263f9e7018c0e0ffbd577c1aa8d47e5
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-04-05 23:08:29 -0400

    Make proper db connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+/**
+ * Connect to the database using the MONGO_URI environment variable
+ */
+export default async function connectDB() {
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined");
+  if (!mongoose.connection || mongoose.connection.readyState === 1) {
+    console.log("Database is already connected");
+    return;
+  }
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+  await mongoose.connect(process.env.MONGO_URI);
+  mongoose.Promise = global.Promise;
+  mongoose.connection.on(
+    "error",
+    console.error.bind(console, "connection error:"),
+  );
+  mongoose.connection.once("open", () => console.log("Database connected"));
+}
