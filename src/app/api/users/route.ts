40e2d54f1edd9d30cd7f73305d95822cf23e1e0e
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-04-06 10:53:54 -0400

    Fix login error

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,7 @@
 import User from "@/db/models/User";
 import catchAsyncError from "@/lib/catchAsyncError";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export const GET = catchAsyncError(async (req) => {
   const users = await User.find();
@@ -9,6 +10,7 @@ export const GET = catchAsyncError(async (req) => {
 
 export const POST = catchAsyncError(async (req) => {
   const userData = await req.json();
+  userData.password = await bcrypt.hash(userData.password, 12);
   await User.create(userData);
   return NextResponse.json({ message: "User created" }, { status: 201 });
 });
